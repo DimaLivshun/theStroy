62ecd25e55a65c404ba0f60b6d3e19e4c374db77
DimaLivshun <lbvf3366@gmail.com>
2018-02-04 23:12:41 +0200

    v01

---
+++ js/common.js
@@ -0,0 +1,125 @@
+$(document).ready(function() {
+
+	//Таймер обратного отсчета
+	//Документация: http://keith-wood.name/countdown.html
+	//<div class="countdown" date-time="2015-01-07"></div>
+	var austDay = new Date($(".countdown").attr("date-time"));
+	$(".countdown").countdown({until: austDay, format: 'yowdHMS'});
+
+	//Попап менеджер FancyBox
+	//Документация: http://fancybox.net/howto
+	//<a class="fancybox"><img src="image.jpg" /></a>
+	//<a class="fancybox" data-fancybox-group="group"><img src="image.jpg" /></a>
+	$(".fancybox").fancybox();
+
+	//Навигация по Landing Page
+	//$(".top_mnu") - это верхняя панель со ссылками.
+	//Ссылки вида <a href="#contacts">Контакты</a>
+	$(".top_mnu").navigation();
+
+	//Добавляет классы дочерним блокам .block для анимации
+	//Документация: http://imakewebthings.com/jquery-waypoints/
+	$(".block").waypoint(function(direction) {
+		if (direction === "down") {
+			$(".class").addClass("active");
+		} else if (direction === "up") {
+			$(".class").removeClass("deactive");
+		};
+	}, {offset: 100});
+
+	//Каруселька
+	//Документация: http://owlgraphic.com/owlcarousel/
+	var owl = $(".carousel");
+	owl.owlCarousel({
+		items : 1,
+		responsive : true,
+		responsiveRefreshRate : 200,
+		itemsCustom : [
+        [0, 1],
+        [450, 1],
+        [600, 1],
+        [700, 1],
+        [1000, 1],
+        [1200, 1],
+        [1400, 1],
+        [1600, 1]
+      ],
+	});
+	/*owl.on("mousewheel", ".owl-wrapper", function (e) {
+		if (e.deltaY > 0) {
+			owl.trigger("owl.prev");
+		} else {
+			owl.trigger("owl.next");
+		}
+		e.preventDefault();
+	});*/
+	/*$(".next_button").click(function(){
+		owl.trigger("owl.next");
+	});
+	$(".prev_button").click(function(){
+		owl.trigger("owl.prev");
+	});*/
+
+	//Кнопка "Наверх"
+	//Документация:
+	//http://api.jquery.com/scrolltop/
+	//http://api.jquery.com/animate/
+	$("#top").click(function () {
+		$("body, html").animate({
+			scrollTop: 0
+		}, 800);
+		return false;
+	});
+	
+	//Аякс отправка форм
+	//Документация: http://api.jquery.com/jquery.ajax/
+	$("form").submit(function() {
+		$.ajax({
+			type: "GET",
+			url: "mail.php",
+			data: $("form").serialize()
+		}).done(function() {
+			alert("Спасибо за заявку!");
+			setTimeout(function() {
+				$.fancybox.close();
+			}, 1000);
+		});
+		return false;
+	});
+
+	//Плавный скролл до блока
+	$(".scroll-button").on("click","a", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+ 
+        //забираем идентификатор блока с атрибута href
+        var id  = $(this).attr('href'),
+ 
+        //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;
+         
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+
+    //Увеличение SVG по скроллу на блок
+    /*function come(elem) {
+	  	var docViewTop = $(window).scrollTop(),
+		    docViewBottom = docViewTop + $(window).height(),
+		    elemTop = $(elem).offset().top,
+		    elemBottom = elemTop + $(elem).height();
+
+			return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+	}
+	if(come('#benefits')){
+		$('.icon1')css.('width','17%').delay(500);
+		$('.icon2')css.('width','17%').delay(1000);
+		$('.icon3')css.('width','17%').delay(1500);
+		$('.icon4')css.('width','17%').delay(2000);
+		$('.icon5')css.('width','17%').delay(2500);
+		$('.icon6')css.('width','17%').delay(3000);
+		$('.icon7')css.('width','17%').delay(3500);
+	}*/
+
+
+});
